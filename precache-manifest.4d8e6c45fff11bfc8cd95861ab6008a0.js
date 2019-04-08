002987f73f20bbd61c443141ee90676062b2c4b9
bartosz4 <bartosz44@gmail.com>
2019-04-09 00:02:54 +0200

    Updates

---
+++ precache-manifest.4d8e6c45fff11bfc8cd95861ab6008a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/WeatherApp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "9dad9032329c2ade5da4",
+    "url": "/WeatherApp/static/js/main.9dad9032.chunk.js"
+  },
+  {
+    "revision": "130cf39c0424556b134c",
+    "url": "/WeatherApp/static/js/1.130cf39c.chunk.js"
+  },
+  {
+    "revision": "9dad9032329c2ade5da4",
+    "url": "/WeatherApp/static/css/main.57f2cb26.chunk.css"
+  },
+  {
+    "revision": "130cf39c0424556b134c",
+    "url": "/WeatherApp/static/css/1.211ef4d1.chunk.css"
+  },
+  {
+    "revision": "084b929951551fbc755b2ab751bdfc49",
+    "url": "/WeatherApp/index.html"
+  }
+];
